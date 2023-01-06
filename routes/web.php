cdfd004d49567620e6bba82aa32d2645305d68bd
Rob Mellett <dev@robmellett.com>
2023-01-06 13:33:03 +0700

    Add cached route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('cache', function () {
+
+    $cached = Cache::remember('cache-test', 10, function () {
+        return Carbon::now();
+    });
+
+    return response()->json([
+        "cached" =>  $cached->diffForHumans(),
+        "now" => Carbon::now()->diffForHumans()
+    ]);
 });
